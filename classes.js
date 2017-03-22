02f196d68978d11616dfa12d9ed4dae3459354e4
andrew-rayco <andy@andycondon.com>
2017-03-22 13:37:48 +1300

    reveal fourth div

--- classes.js
+++ classes.js
@@ -1,23 +1,24 @@
 // Don't change or delete this line! It waits until the DOM has loaded, then calls
 // the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
-document.addEventListener('DOMContentLoaded', start)
+document.addEventListener('DOMContentLoaded', start);
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
   one();
   two();
   three();
+  makeVisible();
   // Your turn! Create a new function called `two`, then call it from here.
-}
+};
 
 function one () {
   // First, we have to find the element:
-  var one = document.getElementById('one')
+  var one = document.getElementById('one');
 
   // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
-}
+  one.classList.add('blue');
+};
 
 // CREATE FUNCTION two HERE
 function two() {
@@ -32,3 +33,7 @@ function three() {
 };
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible() {
+  var invisible = document.getElementsByClassName('invisible');
+  invisible[0].classList.add('visible');
+};
